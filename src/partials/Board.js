757bbd09085d40c53c2f34a34ecbb0b4acac5bcf
johnstonstu <stu.w.johnston@gmail.com>
2018-05-10 11:49:20 -0700

    added game board and lines to js

---
+++ src/partials/Board.js
@@ -0,0 +1,39 @@
+import { SVG_NS } from "../settings";
+
+export default class Board {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  render(svg) {
+    //background of board
+    let rect = document.createElementNS(SVG_NS, "rect");
+    rect.setAttributeNS(null, "fill", "#353535");
+    rect.setAttributeNS(null, "width", this.width);
+    rect.setAttributeNS(null, "height", this.height);
+
+    //middle dash line
+    let line = document.createElementNS(SVG_NS, "line");
+    line.setAttributeNS(null, "x1", this.width / 2);
+    line.setAttributeNS(null, "y1", 0);
+    line.setAttributeNS(null, "x2", this.width / 2);
+    line.setAttributeNS(null, "y2", this.height);
+    line.setAttributeNS(null, "stroke", "white");
+    line.setAttributeNS(null, "stroke-dasharray", "15, 10");
+    line.setAttributeNS(null, "stroke-width", "3");
+
+    //midde dash circle
+    let circle = document.createElementNS(SVG_NS, "circle");
+    circle.setAttributeNS(null, "fill", "#353535");
+    circle.setAttributeNS(null, "cx", this.width / 2);
+    circle.setAttributeNS(null, "cy", this.height / 2);
+    circle.setAttributeNS(null, "r", 45);
+    circle.setAttributeNS(null, "stroke", "white");
+    circle.setAttributeNS(null, "stroke-dasharray", "15, 10");
+    circle.setAttributeNS(null, "stroke-width", "3");
+
+    svg.appendChild(rect);
+    svg.appendChild(line);
+    svg.appendChild(circle);
+  }
+}
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,4 +1,5 @@
 import { SVG_NS } from "../settings";
+import Board from "./Board";
 
 export default class Game {
   constructor(element, width, height) {
@@ -6,15 +7,19 @@ export default class Game {
     this.width = width;
     this.height = height;
     this.gameElement = document.getElementById(this.element);
+    this.board = new Board(this.width, this.height);
   }
 
   render() {
+    //bigfix to clear html
     this.gameElement.innerHTML = "";
-    //renders svg objects
+    //renders basic svg view
     let svg = document.createElementNS(SVG_NS, "svg");
     svg.setAttributeNS(null, "width", this.width);
     svg.setAttributeNS(null, "height", this.height);
     svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
+
+    this.board.render(svg);
   }
 }
