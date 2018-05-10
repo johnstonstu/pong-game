2263cf479c527217935eab84758a2695d71642a6
johnstonstu <stu.w.johnston@gmail.com>
2018-05-10 11:02:31 -0700

    created svg game area in js

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,20 @@
+import { SVG_NS } from "../settings";
+
 export default class Game {
+  constructor(element, width, height) {
+    this.element = element;
+    this.width = width;
+    this.height = height;
+    this.gameElement = document.getElementById(this.element);
+  }
 
-	constructor(element, width, height) {
-		this.element = element;
-		this.width = width;
-		this.height = height;
-	
-		// Other code goes here...
-	}
-
-	render() {
-		// More code goes here...
-	}
-
+  render() {
+    this.gameElement.innerHTML = "";
+    //renders svg objects
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+  }
 }
