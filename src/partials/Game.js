1fe65b27f82a7ef0d72b4c2b3013d183484ab0b6
johnstonstu <stu.w.johnston@gmail.com>
2018-05-10 13:33:44 -0700

    added left/right paddles

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,5 +1,6 @@
 import { SVG_NS } from "../settings";
 import Board from "./Board";
+import Paddle from "./Paddle";
 
 export default class Game {
   constructor(element, width, height) {
@@ -8,6 +9,22 @@ export default class Game {
     this.height = height;
     this.gameElement = document.getElementById(this.element);
     this.board = new Board(this.width, this.height);
+    this.leftPaddle = new Paddle(
+      this.height,
+      this.width / 64,
+      this.height / 4.73,
+      10,
+      100,
+      "red"
+    );
+    this.rightPaddle = new Paddle(
+      this.height,
+      this.width / 64,
+      this.height / 4.73,
+      495,
+      100,
+      "blue"
+    );
   }
 
   render() {
@@ -20,6 +37,11 @@ export default class Game {
     svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
 
+    //renders the board
     this.board.render(svg);
+
+    //render paddles
+    this.leftPaddle.render(svg);
+    this.rightPaddle.render(svg);
   }
 }
